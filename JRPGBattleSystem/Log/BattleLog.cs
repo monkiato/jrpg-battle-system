4533e3235dec570af6cd304f077cf0ef3f681a10
Rodrigo Collavo <rjcollavo@gmail.com>
2022-07-21 20:09:03 +0200

    Add missing Log/ folder

---
+++ JRPGBattleSystem/Log/BattleLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JRPGBattleSystem.Log
+{
+    public class BattleLog<T> where T : Enum
+    {
+        private readonly Queue<BattleLogEntry<T>> entries = new Queue<BattleLogEntry<T>>();
+
+        public BattleLog()
+        {
+        }
+
+        public Queue<BattleLogEntry<T>> GetLogs()
+        {
+            //return a copy
+            return new Queue<BattleLogEntry<T>>(entries.ToArray());
+        }
+
+        public void Push(BattleLogEntry<T> entry)
+        {
+            entries.Enqueue(entry);
+        }
+
+        public void Clean()
+        {
+            entries.Clear();
+        }
+    }
+}
---
+++ JRPGBattleSystem/Log/BattleLogEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace JRPGBattleSystem.Log
+{
+
+    /*
+     * Represents a single battle log report, it's expected to use a specific
+     * Enum to identify the different log types.
+     * 
+     * Usage examples:
+     * 
+     * class MyBattleLogEntry : BattleLogEntry<BattleLogType> { ... }
+     * 
+     * var entry = new MyBattleLogEntry(BattleLogType.Attack)
+     * entry.SetAttacker(attacker)
+     * entry.SetDeffender(deffender)
+     * entry.SetAction(action)
+     * 
+     * new MyBattleLogEntry(BattleLogType.Deffend);
+     * new MyBattleLogEntry(BattleLogType.ModifierRemoved);
+     */
+    public class BattleLogEntry<T> where T : Enum
+    {
+        public BattleLogEntry(T type)
+        {
+            BattleLoGType = type;
+        }
+
+        public T BattleLoGType { get; }
+    }
+}
